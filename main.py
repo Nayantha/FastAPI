00722d50fe7d037de3c26d2b6a6e170b74f94a0b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-02-23 09:16:29 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import logging
+import os
 
-app = FastAPI()
+import telebot
 
-class Msg(BaseModel):
-    msg: str
+from spotdl_bot import upload_files_in_a_folder_to_bot_bot_input
+from spotify_api import get_name_from_link
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+if __name__ == '__main__':
+    os.system("npm install -g spotify-dl")
+    
+    bot_name: str = ""
+    bot_token = os.getenv("BOT_TOKEN")
+    bot_chatId = os.getenv("BOT_CHAT_ID")
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+    logging.info("Starting bot")
+    
+    bot = telebot.TeleBot(token=bot_token, parse_mode=None)
+    
+    
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        bot.reply_to(message, "Howdy, how are you doing?")
+    
+    
+    @bot.message_handler(regexp="spotify")
+    def handle_message(message):
+        dwn_path = "./sp"
+        
+        link = message.text
+        cmd = f"spotifydl {link} --o  {dwn_path}"
+        os.system(cmd)
+        name = get_name_from_link(link=link)
+        bot.reply_to(message, f"{name} downloading...")
+        
+        upload_files_in_a_folder_to_bot_bot_input(bot=bot, path=dwn_path, chat_id=bot_chatId)
+        
+        # https://open.spotify.com/album/4EL83JlSoVsYWbqLJIlxSv?highlight=spotify:track:0i9BCUg1LP9Z0dcRd76bcD
+        # https://open.spotify.com/track/0i9BCUg1LP9Z0dcRd76bcD
+    
+    bot.infinity_polling()
+    
+    
